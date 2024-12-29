212cc98fc43507222565e306ff180766083c87bc
rkkatla <rkatla@Rishabh>
2024-12-29 18:05:17 +0530

    initial commit:

---
+++ download_code.py
@@ -0,0 +1,48 @@
+# import aioboto3
+import asyncio
+import aiohttp
+
+import aiofiles
+
+
+url="https://mirrors.utkarsh2102.org/ubuntu-releases/24.04.1/ubuntu-24.04.1-desktop-amd64.iso"
+
+
+async def download_parts(start,end,i):
+	async with aiohttp.ClientSession() as session:
+		print("starting download for part ",i)
+		headers = {'Range': f'bytes={start}-{end}'}
+		print(headers)
+		async with session.get(url,headers=headers) as response:
+				print(response.status)
+				async with aiofiles.open("ubuntu_"+str(i), mode='wb') as f:
+					# await f.seek(start)
+					async for chunk in response.content.iter_chunked(8192):  # Adjust chunk size if needed
+						await f.write(chunk)
+		print(f"part {i} downloaded")
+
+
+
+async def download_function(parts):
+	async with aiohttp.ClientSession() as session:
+		response = await session.head(url)
+		size=int(response.headers['Content-Length'])
+		print(f"{size=}")
+		chunk_size=size//parts
+		if size%parts==0:
+			pass
+		else:
+			parts+=1
+		print("divided in parts: ",parts)
+		tasks=[]
+		for i in range(parts):
+			start=i*chunk_size
+			end=(i+1)*chunk_size
+			tasks.append(download_parts(start,end,i))
+		await asyncio.gather(*tasks)
+
+
+
+if __name__=="__main__":
+	parts=10
+	asyncio.run(download_function(parts))
