b70654e00b3d6f91b8e0bae94e0405bd755e2976
rkkatla <rkatla@Rishabh>
2024-12-29 18:39:50 +0530

    removed comment in first commit

--- download_code.py
+++ download_code.py
@@ -8,23 +8,22 @@ import aiofiles
 url="https://mirrors.utkarsh2102.org/ubuntu-releases/24.04.1/ubuntu-24.04.1-desktop-amd64.iso"
 
 
-async def download_parts(start,end,i):
-	async with aiohttp.ClientSession() as session:
-		print("starting download for part ",i)
-		headers = {'Range': f'bytes={start}-{end}'}
-		print(headers)
-		async with session.get(url,headers=headers) as response:
-				print(response.status)
-				async with aiofiles.open("ubuntu_"+str(i), mode='wb') as f:
-					# await f.seek(start)
-					async for chunk in response.content.iter_chunked(8192):  # Adjust chunk size if needed
-						await f.write(chunk)
-		print(f"part {i} downloaded")
+async def download_parts(session,start,end,i):
+	print("starting download for part ",i)
+	headers = {'Range': f'bytes={start}-{end}'}
+	print(headers)
+	async with session.get(url,headers=headers) as response:
+			print(response.status)
+			async with aiofiles.open("ubuntu_"+str(i), mode='wb') as f:
+				async for chunk in response.content.iter_chunked(8192):  # Adjust chunk size if needed
+					await f.write(chunk)
+	print(f"part {i} downloaded")
 
 
 
 async def download_function(parts):
-	async with aiohttp.ClientSession() as session:
+	timeout=aiohttp.ClientTimeout(total=None)
+	async with aiohttp.ClientSession(timeout=timeout) as session:
 		response = await session.head(url)
 		size=int(response.headers['Content-Length'])
 		print(f"{size=}")
@@ -38,7 +37,7 @@ async def download_function(parts):
 		for i in range(parts):
 			start=i*chunk_size
 			end=(i+1)*chunk_size
-			tasks.append(download_parts(start,end,i))
+			tasks.append(download_parts(session,start,end,i))
 		await asyncio.gather(*tasks)
 
 
